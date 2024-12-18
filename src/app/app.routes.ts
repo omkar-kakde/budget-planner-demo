a7c25b05055755c2c6409c77ac7c9ed8895f2ac6
omkar <omkarkakde2027@meanacadmey.in>
2024-12-18 21:19:49 +0530

    routes code changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,10 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {path:'budget-planer', loadChildren:()=> import('./budget-planer/budget-planer.module').then(m=> m.BudgetPlanerModule)}
+  { path: '', redirectTo: '/budget-planer/login', pathMatch: 'full' },
+  {
+    path: 'budget-planer',
+    loadChildren: () => import('./budget-planer/budget-planer.module').then(m => m.BudgetPlanerModule)
+  },
+  { path: '**', redirectTo: '/budget-planer/login' } // Catch-all redirect
 ];
--- src/app/budget-planer/budget-planer-routing.module.ts
+++ src/app/budget-planer/budget-planer-routing.module.ts
@@ -10,18 +10,18 @@ import { HistoryComponent } from './history/history.component';
 import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
-  {path:'login' , component:LoginComponent},
-  {path:'side-nav' , component:SideNavComponent},
-  {path: 'dashboard', component: DashboardComponent},
-  {path: 'income', component: IncomeComponent},
-  {path: 'expense', component: ExpenseComponent},
-  {path: 'todo', component: TodoComponent},
-  {path: 'history', component: HistoryComponent},
-  {path: 'profile', component: ProfileComponent}
+  { path: 'login', component: LoginComponent },
+  { path: 'side-nav', component: SideNavComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'income', component: IncomeComponent },
+  { path: 'expense', component: ExpenseComponent },
+  { path: 'todo', component: TodoComponent },
+  { path: 'history', component: HistoryComponent },
+  { path: 'profile', component: ProfileComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class BudgetPlanerRoutingModule { }
+export class BudgetPlanerRoutingModule {}
